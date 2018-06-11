e97c27c1f3a67e71094f2f3a626d94d1123e0d45
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-06-11 17:35:02 +0200

    Add swagger and config for news

---
+++ news/src/main/kotlin/no/exam/news/SwaggerApplication.kt
@@ -0,0 +1,12 @@
+package no.exam.news
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SwaggerApplication
+
+fun main(args: Array<String>) {
+	System.setProperty("spring.profiles.active", "swagger")
+	SpringApplication.run(SwaggerApplication::class.java, *args)
+}
