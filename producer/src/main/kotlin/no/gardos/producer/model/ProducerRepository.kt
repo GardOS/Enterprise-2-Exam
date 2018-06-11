3763c3ebea7568308aed319d5c7db8f9952e4bb7
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-06-11 16:53:29 +0200

    Delete template modules

--- producer/src/main/kotlin/no/gardos/producer/model/ProducerRepository.kt
+++
@@ -1,7 +0,0 @@
-package no.gardos.producer.model
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ProducerRepository : CrudRepository<Producer, Long>
