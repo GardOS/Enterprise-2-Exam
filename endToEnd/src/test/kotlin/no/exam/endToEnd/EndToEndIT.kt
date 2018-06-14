52c67387d15faa8a54e926a5553771b6fa2f620f
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-06-14 07:06:12 +0200

    Add e2e for seller user-story

--- endToEnd/src/test/kotlin/no/exam/endToEnd/EndToEndIT.kt
+++ endToEnd/src/test/kotlin/no/exam/endToEnd/EndToEndIT.kt
@@ -10,15 +10,20 @@ import no.exam.schema.SaleDto
 import no.exam.schema.SellerDto
 import org.awaitility.Awaitility.await
 import org.awaitility.Duration
+import org.h2.util.New
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.*
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
+import org.junit.Assert.*
 import org.testcontainers.containers.DockerComposeContainer
 import java.io.File
 import java.util.concurrent.TimeUnit
 
 class GameFlowIT {
+	val BOOK_PATH = "/book-server/books"
+	val SALE_PATH = "/sale-server/sales"
+	val SELLER_PATH = "/seller-server/sellers"
+	val NEWS_PATH = "/news-server/news"
+
 	companion object {
 		class KDockerComposeContainer(path: File) : DockerComposeContainer<KDockerComposeContainer>(path)
 
@@ -51,13 +56,13 @@ class GameFlowIT {
 		}
 	}
 
-	var initToken : String? = null
-
 	@Before
 	fun init() {
-		initToken = get("/logout")
-				.then()
-				.extract().cookie("XSRF-TOKEN")
+		get("/logout")
+		RestAssured.requestSpecification = RequestSpecBuilder()
+				.setAccept(ContentType.JSON)
+				.setContentType(ContentType.JSON)
+				.build()
 	}
 
 	fun login(username: String, password: String) {
@@ -86,34 +91,38 @@ class GameFlowIT {
 	}
 
 	/*
-	User starts at "login" page. Two buttons "Register" up or "Login".
-	Since this is the first time the user is on the site the "Register" button is clicked
+	This e2e test goes through the authentication features.
+	Follow an user that tests out the register, login, logout, authUser endpoints
 	 */
 	@Test
 	fun authenticationTest() {
-		//User types in user data and clicks OK. This triggers a MQ message that a new user has been created
+		val token = get("/logout")
+				.then()
+				.extract().cookie("XSRF-TOKEN")
+
+		//Register user. This triggers a MQ message that a new user has been created
 		val session = given().contentType(ContentType.URLENC)
 				.formParam("username", "testUser")
 				.formParam("password", "password")
 				.formParam("name", "test name")
 				.formParam("email", "test@test.com")
-				.header("X-XSRF-TOKEN", initToken)
-				.cookie("XSRF-TOKEN", initToken)
+				.header("X-XSRF-TOKEN", token)
+				.cookie("XSRF-TOKEN", token)
 				.post("/register")
 				.then()
 				.statusCode(204)
 				.extract().cookie("SESSION")
 
-		//Session and token is stored in browser
+		//Get session and token
 		RestAssured.requestSpecification = RequestSpecBuilder()
 				.setAccept(ContentType.JSON)
 				.setContentType(ContentType.JSON)
-				.addHeader("X-XSRF-TOKEN", initToken)
-				.addCookie("XSRF-TOKEN", initToken)
+				.addHeader("X-XSRF-TOKEN", token)
+				.addCookie("XSRF-TOKEN", token)
 				.addCookie("SESSION", session)
 				.build()
 
-		//User looks at his user page
+		//User looks at user page
 		get("/authUser")
 				.then()
 				.statusCode(200)
@@ -128,7 +137,7 @@ class GameFlowIT {
 				.then()
 				.statusCode(401)
 
-		//User logs on the account he just created
+		//User logs on the account just created
 		val newSession = given().contentType(ContentType.URLENC)
 				.formParam("username", "testUser")
 				.formParam("password", "password")
@@ -137,14 +146,14 @@ class GameFlowIT {
 				.statusCode(204)
 				.extract().cookie("SESSION")
 
-		//Session stored in browser also reset old values
+		//Session stored, also reset old values
 		RestAssured.requestSpecification = RequestSpecBuilder()
 				.setAccept(ContentType.JSON)
 				.setContentType(ContentType.JSON)
 				.addCookie("SESSION", newSession)
 				.build()
 
-		//User can now look at his user page again
+		//User can now look at user page again
 		val newToken = get("/authUser")
 				.then()
 				.statusCode(200)
@@ -166,79 +175,45 @@ class GameFlowIT {
 				})
 	}
 
-	val BOOK_PATH = "/book-server/books"
-	val SALE_PATH = "/sale-server/sales"
-	val SELLER_PATH = "/seller-server/sellers"
-	val NEWS_PATH = "/news-server/news"
-
-	@Test
-	fun bookCrud() {
-		//Get all books
-		get(BOOK_PATH)
-				.then()
-				.body("size()", equalTo(3))
-				.statusCode(200)
-
-		//Get book by id
-		get("$BOOK_PATH/1")
-				.then()
-				.statusCode(200)
-
-		val newBook = BookDto(title = "Title", author = "Author", edition = "5th")
-
-		//Create book, but needs to be logged in
-		given().body(newBook)
-				.post(BOOK_PATH)
-				.then()
-				.statusCode(401)
-
-		login("user", "pwd")
-
-		//Authorized
-		given().body(newBook)
-				.post(BOOK_PATH)
-				.then()
-				.statusCode(201)
-
-		//
-		get(BOOK_PATH)
-				.then()
-				.body("size()", equalTo(4))
-				.statusCode(200)
-	}
-
+	/*
+	This e2e test goes through features relevant for when a user discovers a book for sale and the proceeds to buy it.
+	 */
 	@Test
 	fun buyerUserStory() {
-//		User checks latest news from news-feed and finds a specific news interesting
+		val token = get("/logout")
+				.then()
+				.extract().cookie("XSRF-TOKEN")
+
+		//User checks latest news from news-feed and finds a specific news interesting
 		val news = given().param("getLatest", true)
 				.get(NEWS_PATH)
 				.then()
 				.statusCode(200)
 				.extract().body()
-				.`as`(Array<NewsDto>::class.java).first { n -> n.sale == 3L }
+				.`as`(Array<NewsDto>::class.java).first { n -> n.bookPrice == 700 }
 
-//		Get sale by id
+		//User checks out sale details
 		val sale = get("$SALE_PATH/${news.sale}")
 				.then()
 				.statusCode(200)
 				.extract()
 				.`as`(SaleDto::class.java)
 
-//		Get book related to sale
+		//User checks out book details
 		val book = get("$BOOK_PATH/${sale.book}")
 				.then()
 				.statusCode(200)
 				.extract()
 				.`as`(BookDto::class.java)
 
-//		Check all sales for book. Finds a cheaper one
+		//User checks if there are any better offers. Finds one
 		val cheaperSale = get("$SALE_PATH/books/${book.id}")
 				.then()
 				.statusCode(200)
 				.extract().body()
 				.`as`(Array<SaleDto>::class.java).first { s -> s.price == 300 }
 
-//		Find better deal on book, check out seller
+		//User checks out the seller selling the book
 		val seller = get("$SELLER_PATH/${cheaperSale.seller}")
 				.then()
 				.statusCode(200)
@@ -246,14 +221,16 @@ class GameFlowIT {
 				.`as`(SellerDto::class.java)
 
 
-		//Curious if seller got other good deals. Does not
+		//User is curious if seller got other good deals and checks. Found none interesting
 		get("$SALE_PATH/sellers/${seller.username}")
 				.then()
 				.statusCode(200)
 
+		//Book is sold
+
 		//Seller tries to remove sale. Forgot to log in. Token included for accurate response
-		given().header("X-XSRF-TOKEN", initToken)
-				.cookie("XSRF-TOKEN", initToken)
+		given().header("X-XSRF-TOKEN", token)
+				.cookie("XSRF-TOKEN", token)
 				.delete("$SALE_PATH/${cheaperSale.id}")
 				.then()
 				.statusCode(401)
@@ -293,6 +270,148 @@ class GameFlowIT {
 							.statusCode(200)
 							.extract().`as`(SellerDto::class.java)
 							.sales!!.find { equals(cheaperSale.id) }
+					)
+					true
+				})
+	}
+
+	/*
+	This e2e test goes through features relevant for when a user wants to sell a book
+	*/
+	@Test
+	fun sellerUserStory() {
+		//Get token
+		val token = get("/logout")
+				.then()
+				.extract().cookie("XSRF-TOKEN")
+
+		//User finds book already in system,
+		val sale = SaleDto(
+				book = 2,
+				price = 1234,
+				condition = "Perfect"
+		)
+
+		//User creates sale for book. Forgot to log in
+		given().header("X-XSRF-TOKEN", token)
+				.cookie("XSRF-TOKEN", token)
+				.body(sale).post(SALE_PATH)
+				.then()
+				.statusCode(401)
+
+		//User logs in
+		login("user", "pwd")
+
+		//Unstable in test, retry a bit
+		await().atMost(
+				20, TimeUnit.SECONDS)
+				.pollInterval(Duration.ONE_SECOND)
+				.ignoreExceptions()
+				.until({
+					//Creates sale again not that user is logged in
+					given().body(sale).post(SALE_PATH)
+							.then()
+							.statusCode(201)
+					true
+				})
+
+		//Refresh sale, now with ID
+		val newSale = get(SALE_PATH)
+				.then()
+				.statusCode(200)
+				.extract()
+				.body().`as`(Array<SaleDto>::class.java).last()
+
+		//Verify that we got the correct sale. Bit overkill, but gives MQ time to get/send messages
+		assertEquals(sale.book, newSale.book)
+		assertEquals(sale.price, newSale.price)
+		assertEquals(sale.condition, newSale.condition)
+
+		//Verify seller added sale
+		val seller = get("$SELLER_PATH/${newSale.seller}")
+				.then()
+				.statusCode(200)
+				.extract().body()
+				.`as`(SellerDto::class.java)
+
+		assert(seller.sales!!.contains(newSale.id))
+
+		//Verify that news have been created for the sale
+		var news: NewsDto? = null
+		await().atMost(
+				10, TimeUnit.SECONDS)
+				.pollInterval(Duration.ONE_SECOND)
+				.ignoreExceptions()
+				.until({
+					//News updated
+					news = get(NEWS_PATH)
+							.then()
+							.statusCode(200)
+							.extract().body()
+							.`as`(Array<NewsDto>::class.java).find { n -> n.sale == newSale.id }
+					assertNotNull(news)
+					true
+				})
+
+		//Time goes on, nobody buys the book. Book deteriorates, seller updates condition and price
+		given().body(SaleDto(price = 123, condition = "Bad"))
+				.patch("$SALE_PATH/${newSale.id}")
+				.then()
+				.statusCode(204)
+
+		//Wait for MQ-message
+		var updatedNews: NewsDto? = null
+		await().atMost(
+				10, TimeUnit.SECONDS)
+				.pollInterval(Duration.ONE_SECOND)
+				.ignoreExceptions()
+				.until({
+					//News updated
+					updatedNews = get(NEWS_PATH)
+							.then()
+							.statusCode(200)
+							.extract().body()
+							.`as`(Array<NewsDto>::class.java).find { n -> n.sale == newSale.id }
+					assertNotNull(updatedNews)
+					true
+				})
+
+
+		assertNotEquals(news?.bookPrice, updatedNews?.bookPrice)
+		assertNotEquals(news?.bookCondition, updatedNews?.bookCondition)
+
+		//Buyer sees cheap book and buys it. Seller removes sale
+		delete("$SALE_PATH/${newSale.id}")
+				.then()
+				.statusCode(204)
+
+		//Wait for MQ-message
+		await().atMost(
+				10, TimeUnit.SECONDS)
+				.pollInterval(Duration.ONE_SECOND)
+				.ignoreExceptions()
+				.until({
+					//Verify news is removed from news-feed
+					assertNull(get(NEWS_PATH)
+							.then()
+							.statusCode(200)
+							.extract().body()
+							.`as`(Array<NewsDto>::class.java).find { n -> n.sale == newSale.id }
+					)
+					true
+				})
+
+		//Wait for MQ-message then
+		await().atMost(10, TimeUnit.SECONDS)
+				.pollInterval(Duration.ONE_SECOND)
+				.ignoreExceptions()
+				.until({
+					//Verify sale is removed from seller
+					assertNull(get("$SELLER_PATH/${seller.username}")
+							.then()
+							.statusCode(200)
+							.extract().`as`(SellerDto::class.java)
+							.sales!!.find { equals(newSale.id) }
 					)
 					true
 				})
